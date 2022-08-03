45db245faff41fa94b9732bc5ec5d17ec719271b
RomanKarasev <roman.karasev@icloud.com>
2022-08-03 20:09:20 +0300

    Initial Commit

---
+++ Let's Do ItUITests/Let_s_Do_ItUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Let_s_Do_ItUITestsLaunchTests.swift
+//  Let's Do ItUITests
+//
+//  Created by Роман Карасёв on 03.08.2022.
+//
+
+import XCTest
+
+class Let_s_Do_ItUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
